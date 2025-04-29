5182370a073d7d5a387ce7fdd70e31729b8aeb8c
Anas Chanek <achanek@c2r1p7.1337.ma>
2025-04-29 09:00:28 +0100

    i added the form of the headers

---
+++ includes/minishell.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: achanek <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/28 11:28:50 by achanek           #+#    #+#             */
+/*   Updated: 2025/04/28 11:35:58 by achanek          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+#define MINISHELL_H
+
+#include "builtins.h"
+#include "executor.h"
+#include "parser.h"
+#include "signals.h"
+
+
+#endif
