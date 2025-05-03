910f582f9138e25fd4a4bb252d38e589205305b4
khalid-elbh <khalidelbahry33@gmail.com>
2025-05-03 17:42:10 +0100

    go

--- src/ft_split.c
+++ src/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: khalidelbahry <khalidelbahry@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 14:38:57 by kel-bahr          #+#    #+#             */
-/*   Updated: 2025/05/03 16:31:01 by khalidelbah      ###   ########.fr       */
+/*   Updated: 2025/05/03 17:41:09 by khalidelbah      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,8 @@ int ft_cont_word(char *str)
 		count++;
 	while (str[i])
 	{
-		if (str[i] != ' ' && str[i])
-			count++;
+		while (str[i] != ' ' && str[i] && str[i] != '"' && str[i] != '\'')
+			i++;
 		if (str[i] == '"')
 		{
 			while (str[i])
@@ -40,22 +40,21 @@ int ft_cont_word(char *str)
 			{
 				i++;
 				if (str[i] == '\'')
-					break;
+				break;
 			}
 			i++;
 		}
 		while (str[i] && str[i] == ' ' && str[i] != '"' && str[i] != '\'')
 			i++;
-		while (str[i] != ' ' && str[i] && str[i] != '"' && str[i] != '\'')
-			i++;
+		if (str[i] != ' ' && str[i])
+			count++;
 	}
 	return(count);
 }
 
-char **split(char *str)
-{
-	char **line;
+// char **split(char *str)
+// {
+// 	char **line;
 	
-	line = malloc (ft_cont_word(str) + 1);
-	 
-}
+// 	line = malloc (ft_cont_word(str) + 1);
+// }
