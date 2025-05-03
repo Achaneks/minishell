0056d6f1c0dfa85af3ff7d5c2bcc98db0c902447
khalid-elbh <khalidelbahry33@gmail.com>
2025-05-03 16:35:53 +0100

    go

--- Makefile
+++ Makefile
@@ -1,32 +1,16 @@
 NAME = minishell
 
-<<<<<<< HEAD
 SRC = src/main.c src/ft_split.c
 
 OBJ = $(SRC:.c=.o)
 
 CC = cc
-CFLAGS = -Wall -Wextra -Werror
-=======
-SRC = src/main.c
-OBJ = $(SRC:.c=.o)
-
-CC = cc
-CFLAGS = -Wall -Wextra -Werror -Iincludes
-LDFLAGS = -lreadline
->>>>>>> f4cd3977161ea9e23c4a409204ccd8fbd4bf528c
+CFLAGS = -Wall -Wextra -Werror -g
 
 all: $(NAME)
 
 $(NAME): $(OBJ)
-<<<<<<< HEAD
 	$(CC) $(CFLAGS) -o $(NAME) $(OBJ) -lreadline
-=======
-	$(CC) $(CFLAGS) $(OBJ) -o $(NAME) $(LDFLAGS)
-
-%.o: %.c
-	$(CC) $(CFLAGS) -c $< -o $@
->>>>>>> f4cd3977161ea9e23c4a409204ccd8fbd4bf528c
 
 clean:
 	rm -f $(OBJ)
--- src/ft_split.c
+++ src/ft_split.c
@@ -3,54 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kel-bahr <kel-bahr@student.42.fr>          +#+  +:+       +#+        */
+/*   By: khalidelbahry <khalidelbahry@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 14:38:57 by kel-bahr          #+#    #+#             */
-/*   Updated: 2025/04/30 14:39:26 by kel-bahr         ###   ########.fr       */
+/*   Updated: 2025/05/03 16:31:01 by khalidelbah      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/parser.h"
 
-// int ft_cont_word(char *str)
-// {
-// 	int i;
-// 	int count;
-// 	int flag;
-// 	int f2;
-
-// 	f2 = 0;
-// 	flag = 0;
-// 	i = 0;
-// 	count = 0;
-// 	while(str[i])
-// 	{
-// 		if (str[i] == '"' && flag == 0)
-// 			flag = 1;
-// 		else if (str[i] == '"' && flag == 1)
-// 			flag = 0;
-// 		if (str[i] == '\'' && f2 == 0)
-// 			f2 = 1;
-// 		else if (str[i] == '\'' && f2 == 1)
-// 			f2 = 0;
-// 		if(str[i] == ' ')
-// 		{
-// 			if (!flag || !f2)
-// 				count++;
-// 		}
-// 		i++;
-// 	}
-// 	return (count);
-// }
-
 int ft_cont_word(char *str)
 {
 	int i = 0, count = 0;
-
+	
+	while (str[i] && str[i] == ' ')
+		i++;
+	if (str[i] != ' ' && str[i])
+		count++;
 	while (str[i])
 	{
-		while (str[i] && str[i] == ' ')
-			i++;
 		if (str[i] != ' ' && str[i])
 			count++;
 		if (str[i] == '"')
@@ -61,6 +32,7 @@ int ft_cont_word(char *str)
 				if (str[i] == '"')
 					break;
 			}
+			i++;
 		}
 		if (str[i] == '\'')
 		{
@@ -70,21 +42,20 @@ int ft_cont_word(char *str)
 				if (str[i] == '\'')
 					break;
 			}
+			i++;
 		}
-		if (str[i] == ' ')
-		{
-			count++;
-			while (str[i] && str[i] == ' ')
-				i++;
-		}
-		while (str[i] != ' ' && str[i])
+		while (str[i] && str[i] == ' ' && str[i] != '"' && str[i] != '\'')
+			i++;
+		while (str[i] != ' ' && str[i] && str[i] != '"' && str[i] != '\'')
 			i++;
 	}
 	return(count);
 }
 
-// char **split(char *str)
-// {
-// 	char **line;
-
-// }
+char **split(char *str)
+{
+	char **line;
+	
+	line = malloc (ft_cont_word(str) + 1);
+	 
+}
